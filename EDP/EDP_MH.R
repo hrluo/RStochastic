8bc7d6cde1336b9a25c6c4f217d7d82e82103e5c
Henry.L <4islands@gmail.com>
2017-10-14 15:25:24 -0400

    Enriched Dirichlet Process with MCMC
    Enriched Dirichlet Process with MCMC

---
+++ EDP/EDP_MH.R
@@ -0,0 +1,52 @@
+trueA <- 5
+trueB <- 0
+trueSd <- 10
+sampleSize <- 31
+
+# create independent x-values 
+x <- (-(sampleSize-1)/2):((sampleSize-1)/2)
+# create dependent values according to ax + b + N(0,sd)
+y <-  trueA * x + trueB + rnorm(n=sampleSize,mean=0,sd=trueSd)
+
+plot(x,y, main="Test Data")
+likelihood <- function(param){
+  a = param[1]
+  b = param[2]
+  sd = param[3]
+  
+  pred = a*x + b
+  singlelikelihoods = dnorm(y, mean = pred, sd = sd, log = T)
+  sumll = sum(singlelikelihoods)
+  return(sumll)   
+}
+
+posterior <- function(param){
+  return (likelihood(param) + prior(param))
+}
+######## Metropolis algorithm ################
+
+proposalfunction <- function(param){
+  return(rnorm(3,mean = param, sd= c(0.1,0.5,0.3)))
+}
+
+run_metropolis_MCMC <- function(startvalue, iterations){
+  chain = array(dim = c(iterations+1,3))
+  chain[1,] = startvalue
+  for (i in 1:iterations){
+    proposal = proposalfunction(chain[i,])
+    
+    probab = exp(posterior(proposal) - posterior(chain[i,]))
+    if (runif(1) < probab){
+      chain[i+1,] = proposal
+    }else{
+      chain[i+1,] = chain[i,]
+    }
+  }
+  return(chain)
+}
+
+startvalue = c(4,0,10)
+chain = run_metropolis_MCMC(startvalue, 10000)
+
+burnIn = 5000.
+acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
